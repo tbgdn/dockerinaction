1b6bc5e5c9e27395c0b2455ee8a6fc0aedbf96be
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2020-01-17 23:13:27 +0200

    Automate an environment of 3 docker images

--- ch02.py
+++ ch02.py
@@ -1,10 +1,60 @@
 import subprocess
+from time import sleep
 
-result = subprocess.run(
-    ['echo', 'Hello from the child!'],
-    capture_output=True,
-    shell=True,
-    encoding='utf-8')
+def printCommandId(command, id):
+    print('{0} with id: {1}'.format(command, id))
 
-result.check_returncode()  # No exception means clean exit
-print(result.stdout)
+def dockerRun(args):
+    return docker(['run'] + args)
+
+def dockerStart(args):
+    return docker(['start'] + args)
+
+def docker(args):
+    return runCommand(['docker'] + args)
+
+def runCommand(args):
+    result = subprocess.run(
+        args,
+        capture_output=True,
+        shell=True,
+        encoding='utf-8')
+    if (result.returncode):
+        print(result.stderr)
+        print(result.stdout)
+        result.check_returncode()
+
+    res = result.stdout
+    return res.strip().strip("\"")
+
+def removeAllContainers():
+    print('Cleaning up Docker...')
+    containerIds = docker(['ps', '-a', '-q'])
+    for id in containerIds.split('\n'):
+        printCommandId('stopping', id)
+        id = docker(['stop', id])
+        printCommandId('removing', id)
+        id = docker(['rm', id])
+        printCommandId('removed', id)
+
+def runChapter():
+    mailerCid = dockerRun(['-d', 'dockerinaction/ch2_mailer'])
+    print('running %s' % mailerCid)
+    webCid = docker(['create', 'nginx'])
+    printCommandId('created', webCid)
+    agentCid = docker(['create', '--link', '%s:insideweb' % webCid, '--link', '%s:insidemailer' % mailerCid, 'dockerinaction/ch2_agent'])
+    print('created %s' % agentCid)
+    webRid = dockerStart([webCid])
+    agentRid = dockerStart([agentCid])
+    printCommandId('running', webRid)
+    print('running %s' % agentRid)
+
+try:
+    timeout = 10
+    runChapter()
+    print('Running for {0}s'.format(timeout))
+    sleep(timeout)
+    removeAllContainers()
+except subprocess.CalledProcessError as err:
+    print('Some error was thrown while executing docker command:\n{0}'.format(err))
+    removeAllContainers()
---
+++ playground.py
@@ -0,0 +1,5 @@
+# This might contain small snippets to check python syntax
+
+args = ['1', '2', '3']
+
+print(args[1:])
