d84181d483e584fafda133a5b499a61fe6371694
Teslarasu Bogdan <teslarasu.bogdan@gmail.com>
2020-01-27 20:12:50 +0200

    Fix the implementation images
    Because of windows adding an extra \r, the bash scripts used
    by images cannot be executed. With the help of `cat -v ${file}`
    and some `tr -d "\r"` magic, the extra characters were removed
    and the implementation images can execute the scripts

--- resources/ch8/mailer-base/log-impl/mailer.sh
+++ resources/ch8/mailer-base/log-impl/mailer.sh
@@ -1,8 +1,8 @@
-#./bin/bash
-printf "Logging Mailer has start.\n"
+#!/bin/bash
+printf "Logging Mailer has started.\n"
 while true
 do
-  MESSAGE=$(nc -l -p 33333)
-  printf "[Message]: %s\n" "$MESSAGE" > $1
-  sleep 1
+	MESSAGE=$(nc -l -p 33333)
+	printf "[Message]: %s\n" "$MESSAGE" > 1
+	sleep 1
 done
